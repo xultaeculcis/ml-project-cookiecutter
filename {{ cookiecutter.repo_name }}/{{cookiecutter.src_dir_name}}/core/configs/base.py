514a8ef19d37b8a5ad23597bd5851ee4427182c6
Michał Wierzbiński <19872529+xultaeculcis@users.noreply.github.com>
2024-04-15 16:48:34 +0200

    Fix config logging (#13)
    * Fix bad logging arg passing
    * Remove useless `log_self()` method

--- {{ cookiecutter.repo_name }}/{{cookiecutter.src_dir_name}}/core/configs/base.py
+++ {{ cookiecutter.repo_name }}/{{cookiecutter.src_dir_name}}/core/configs/base.py
@@ -2,17 +2,9 @@ from __future__ import annotations
 
 from pydantic import BaseModel
 
-from {{cookiecutter.src_dir_name}}.utils.logging import get_logger
-
-_logger = get_logger("config")
-
 
 class ConfigBase(BaseModel):
     """A base class for all entrypoint config classes."""
 
     def __str__(self) -> str:
         return self.model_dump_json(indent=4)  # type: ignore[no-any-return]
-
-    def log_self(self) -> None:
-        """Logs a short info with INFO logging level about what parameters is the script being run with."""
-        _logger.info("Running with following config: $(cfg)", extra={"cfg": self})
